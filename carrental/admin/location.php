c09f03d46de5b300e74ec80d20f2393105d22c55
Rajeev Kushwaha <rk7940271@gmail.com>
2023-04-23 23:47:47 +0530

    track vehicles system added

---
+++ carrental/admin/location.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3410.768607180183!2d75.69758687480224!3d31.254828360214823!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391a5fb2dc210389%3A0xb5f241ec99d46ad9!2sLPU%20Law%20Gate!5e0!3m2!1sen!2sin!4v1682187143546!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</body>
+
+</html>
