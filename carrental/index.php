263c389b822744e7ed6bc5cde6caf2688196454e
Rajeev Kushwaha <rk7940271@gmail.com>
2023-04-26 00:06:13 +0530

    about us updated

--- carrental/index.php
+++ carrental/index.php
@@ -71,10 +71,7 @@ error_reporting(0);
         <div class="container">
             <div class="section-header text-center">
                 <h2>Find the Best <span>CarForYou</span></h2>
-                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered
-                    alteration in some form, by injected humour, or randomised words which don't look even slightly
-                    believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't
-                    anything embarrassing hidden in the middle of text.</p>
+                <p>We have some new cars, choose the car that meets your requirements.</p>
             </div>
             <div class="row">
 
